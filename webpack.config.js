ba0402319a42660fe7ebdca32964b8094ddc072d
Kevin J Donohue <kevinjdonohue@yahoo.com>
2017-12-29 18:18:32 -0800

    updated output path to use path.resolve instead of a raw concatenation

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   entry: ["./src/index.js"],
   output: {
-    path: __dirname + "/dist",
+    path: path.resolve(__dirname, "/dist"),
     publicPath: "/",
     filename: "bundle.js"
   },
