181ea3e6b43aec6ad2b6ad03d163894bcc25f4e7
Kevin J Donohue <kevinjdonohue@yahoo.com>
2017-12-30 20:01:09 -0800

    adding globals via env settings so that eslint doesn't complain about globals, e.g. document, window, etc.
    installing babel package and adding configuration to package.json in order to enable necessary presets
    
    installing react and react-dom packages
    
    adding module loaders to webpack config to allow hotloading for .js and .jsx files

--- src/index.js
+++
@@ -1,4 +0,0 @@
-/* eslint-disable no-console */
-console.log('My Minimal React Webpack Babel Setup!!');
-
-module.hot.accept();
--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,20 @@ module.exports = {
   entry: [
     'webpack-dev-server/client?http://localhost:8080',
     'webpack/hot/only-dev-server',
-    './src/index.js',
+    './src/index.jsx',
   ],
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'react-hot-loader/webpack!babel-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
   output: {
     path: path.resolve(__dirname, '/dist'),
     publicPath: '/',
