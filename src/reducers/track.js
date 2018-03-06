d05235b1b3bac7960f2bebeb0ba624f3d54b0eec
Kevin J Donohue <kevinjdonohue@yahoo.com>
2018-03-05 22:29:50 -0800

    adding our first action creator and reducer as part of starting to implement redux

---
+++ src/reducers/track.js
@@ -0,0 +1,18 @@
+import * as actionTypes from '../constants/actionTypes';
+
+const initialState = [];
+
+function setTracks(state, action) {
+  const { tracks } = action;
+
+  return [...state, ...tracks];
+}
+
+export default function (state = initialState, action) {
+  switch (action.type) {
+    case actionTypes.TRACKS_SET:
+      return setTracks(state, action);
+    default:
+      return state;
+  }
+}
